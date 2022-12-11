fee5a5c79ec3c7c5ceeea8a1b8d2609b3a53bbdb
beginor <beginor@qq.com>
2022-12-11 16:15:41 +0800

    update package version, update package release notes

--- push-package.sh
+++ push-package.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 dotnet pack src/AspNetCore.Authentication.Token/AspNetCore.Authentication.Token.csproj -c Release
-dotnet nuget push src/AspNetCore.Authentication.Token/bin/Release/Beginor.AspNetCore.Authentication.Token.6.0.1.nupkg -s nuget.org -k $(cat ~/.nuget/key.txt)
+dotnet nuget push src/AspNetCore.Authentication.Token/bin/Release/Beginor.AspNetCore.Authentication.Token.7.0.0.nupkg -s nuget.org -k $(cat ~/.nuget/key.txt)
 rm src/AspNetCore.Authentication.Token/bin/Release/*.nupkg
