4ce9270886b8469bce16ddcb89c945d41b693f68
beginor <beginor@qq.com>
2023-11-19 11:28:29 +0800

    update to .NET 8.0.0

--- push-package.sh
+++ push-package.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
+VERSION="8.0.0"
 dotnet pack src/AspNetCore.Authentication.Token/AspNetCore.Authentication.Token.csproj -c Release
-dotnet nuget push src/AspNetCore.Authentication.Token/bin/Release/Beginor.AspNetCore.Authentication.Token.7.0.0.nupkg -s nuget.org -k $(cat ~/.nuget/key.txt)
+dotnet nuget push src/AspNetCore.Authentication.Token/bin/Release/Beginor.AspNetCore.Authentication.Token.$VERSION.nupkg -s nuget.org -k $(cat ~/.nuget/key.txt)
 rm src/AspNetCore.Authentication.Token/bin/Release/*.nupkg
